3b8cfb39236fc1c69bffe3851ab796d9b31df497
Michael Ballard <ballard.michael9@gmail.com>
2018-05-07 23:34:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+  badge = "Hello, my name is #{name}."
+  badge
+end
+
+
+def batch_badge_creator(array)
+  badges = [ ]
+  array.each do |x|
+    badges.push("Hello, my name is #{x}.")
+  end
+  return badges 
+end
+
+def assign_rooms(array)
+  room = [ ]
+  for i in 0..6
+    room[i] = "Hello, #{array[i]}! You'll be assigned to room #{i+1}!"
+  end
+  return room
+end
+
+def printer()
